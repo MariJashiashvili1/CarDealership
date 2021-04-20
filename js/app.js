9324bf9028b7404b8e68a34a2d182d58615b6937
MariamJashiashvili <mari.jashiashvili111@gmail.com>
2021-04-20 21:32:14 +0400

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+window.addEventListener('load', () => document.querySelector('.preloader').classList.add('hidePreloader'));
+
+const CreateCars = (() => {
+    // car data
+    const cars = [];
+
+    class Car {
+        constructor(make, country, img, special, model, price, type, trans, gas) {
+            this.make = make;
+            this.country = country;
+            this.img = img;
+            this.special = special;
+            this.model = model;
+            this.price = price;
+            this.type = type;
+            this.trans = trans;
+            this.gas = gas;
+        }
+    }
+})();
